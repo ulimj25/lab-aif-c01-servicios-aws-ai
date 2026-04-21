53c87223efa6fd7ac23a85963cc09f869bfa6ecc
De4dS1 <ulihmj2503@gmail.com>
2026-04-21 13:27:34 -0600

    Primera parte del ejercicio 5

--- ejercicios/ex05_audio_pipeline.py
+++ ejercicios/ex05_audio_pipeline.py
@@ -34,4 +34,65 @@ Relacion con AIF-C01:
 import boto3
 import json
 
-# Tu codigo aqui
+from pathlib import Path
+
+# Configuración
+bucket = "audio-pipeline-2026-04-21-12-40" 
+region = "us-east-1"
+polly_voz = "Mia"
+formato = "mp3"
+engine = "neural"
+
+# Carga de las noticias
+with open('data/noticias.json', 'r', encoding='utf-8') as f:
+    noticias = json.load(f)
+
+# Servicios AWS
+
+polly = boto3.client("polly", region_name=region)
+s3 = boto3.client("s3", region_name=region)
+transcribe = boto3.client("transcribe", region_name=region)
+
+# Obtener audio-stream de Polly
+
+def sintetizar_speech(texto, voz_id=polly_voz, metodo=engine):
+
+    respuesta = polly.synthesize_speech(
+        Engine = metodo,
+        VoiceId = voz_id,
+        OutputFormat = formato,
+        Text = texto
+    )
+
+    audio_bytes = respuesta["AudioStream"].read()
+
+    print("Audio Generado:", len(audio_bytes)/1024)
+
+    return audio_bytes
+
+# Guardar el audio
+
+def guardar_audio(audio_bytes, nombre):
+
+    titulo_limpio = "".join(car if car.isalnum() else "_" for car in nombre)
+
+    directorio = Path("data/output/"+titulo_limpio+".mp3")
+
+    directorio.write_bytes(audio_bytes)
+
+    return directorio
+
+def pipeline_audio(noticias, nombre):
+
+    titulo = noticias[0]['titulo']
+    noticia = noticias[0]['texto']
+
+    texto = f"{titulo}{": "}{noticia}"
+
+    bytes_audio = sintetizar_speech(texto)
+
+    archivo_audio = guardar_audio(bytes_audio, nombre)
+
+    print("Archivo guardado en: ", archivo_audio)
+
+pipeline_audio(noticias, "noticia_1_speech")
